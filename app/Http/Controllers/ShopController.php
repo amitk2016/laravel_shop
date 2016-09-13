0bd34fb79b83d785066bd011bc8c9b01e00c5c87
Amit Kumar <amit.k03377@gmail.com>
2016-09-14 11:25:32 +1200

    finished adding new  productin the database

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Products;
 
 class ShopController extends Controller
 {
@@ -22,11 +23,24 @@ class ShopController extends Controller
 
     		'product_title' => 'required|min:1|max:255',
     		'product_description' => 'required|min:10',
-            'amount' => 'required|number',
+            'amount' => 'required|numeric',
             'quantity' =>'required|number',
             'image' =>'required|image',
 
 
     	]);
+
+        $newProduct = new Products();
+        $newProduct->title = $request->product_title;
+        $newProduct->description = $request->product_description;
+        $newProduct->price = $request->price;
+        $newProduct->quantity = $request->quantity;
+
+
+        // $newProduct->save(); 
+        return redirect('/shop');
+
+        // var_dump($newProduct);
+        // die();
     }
 }
